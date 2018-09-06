1dfa0958a06a7087818de69c5b0fcb2e8ef4c100
RashidMall <mr.mallaev@gmail.com>
2018-09-07 01:15:43 +0200

    Rearange form's attributes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -52,15 +52,9 @@ class BlogController extends AbstractController
         $article = new Article();
 
         $form = $this->createFormBuilder($article)
-            ->add('title', TextType::class, ['attr' => [
-                'placeholder' => "Title",
-                ]])
-            ->add('content', TextareaType::class, ['attr' => [
-                'placeholder' => "Content",
-                ]])
-            ->add('image', TextType::class, ['attr' => [
-                'placeholder' => "Image",
-                ]])
+            ->add('title')
+            ->add('content')
+            ->add('image')
             ->getForm();
 
         return $this->render('blog/new.html.twig', [
