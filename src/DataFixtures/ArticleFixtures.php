73727ae38f904e8095cac988ff00accd7f4e1c0d
RashidMall <mr.mallaev@gmail.com>
2018-09-08 17:31:06 +0200

    Add entities: category and comment

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,19 +5,33 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++){
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = Faker\Factory::create();
 
-            $manager->persist($article);
+        // Create fake categories
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Create fake articles
+            for($j = 1; $j <= 10; $j++){
+                $article = new Article();
+                $article->setTitle($faker->sentence())
+                        ->setContent('<p>' . join($faker->paragraphs(4), '</p><p>') . '</p>')
+                        ->setImage("http://placehold.it/350x150")
+                        ->setCreatedAt(new \DateTime());
+    
+                $manager->persist($article);
+            }
         }
 
         $manager->flush();
