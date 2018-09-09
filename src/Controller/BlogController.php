735db874f07ac9b94bc65907525e88b7896e1b1e
RashidMall <mr.mallaev@gmail.com>
2018-09-09 21:54:11 +0200

    Add comment an article form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Repository\ArticleRepository;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,9 +42,24 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show", requirements={"id"="\d+"})
      */
-    public function show(Article $article){
+    public function show(Article $article, Request $request, ObjectManager $manager){
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/show.html.twig', [
-            'article' => $article
+            'article' => $article,
+            'formComment' => $form->createView()
         ]);
     }
 
